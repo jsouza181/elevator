3f942fd07eaaa91ec5b5d4c757fc67263bcdd1f3
Jason <jsouza181@yahoo.com>
2015-11-02 16:51:10 -0500

    Delted unused object files

--- part2/.my_xtime.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/usr/src/test_kernel/elevator/part2/my_xtime.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /usr/src/test_kernel/elevator/part2/my_xtime.ko /usr/src/test_kernel/elevator/part2/my_xtime.o /usr/src/test_kernel/elevator/part2/my_xtime.mod.o
--- part2/.tmp_versions/my_xtime.mod
+++
@@ -1,2 +0,0 @@
-/usr/src/test_kernel/elevator/part2/my_xtime.ko
-/usr/src/test_kernel/elevator/part2/my_xtime.o
