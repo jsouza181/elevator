95effba83cf69f9694f0485ea8d9af0fc1547231
Ricardo Castilla <castillaricardo19@gmail.com>
2015-11-01 22:15:15 -0500

    added git ignore to stop getting object files

--- src/scheduler.c
+++ src/scheduler.c
@@ -6,32 +6,39 @@
 #include <linux/slab.h>
 #include "elevator.h"
 
-// Threaded function to handle elevator movement.
+
+// Add passenger's desired floor to the list of scheduled floors
+void scheduleRequest(int requestFloor) {
+  RequestNode *newRequest;
+
+  // Add the new request floor to the queue of floors to visit.
+  newRequest = kmalloc(sizeof(RequestNode), __GFP_WAIT | __GFP_IO | __GFP_FS);
+  newRequest->floorNum = requestFloor;
+  printk("Memory for newRequest successfully allocated\n");
+  list_add_tail(&newRequest->requestList, &requestQueue);
+  printk("newRequest added to request queue\n");
+}
+
+
 int serviceRequests(void *data) {
+  RequestNode *nextFloorToVisit;
   ssleep(1);
 
   while(!kthread_should_stop()) {
-    // Determine the next floor to visit
-    osMagicElv.nextFloor = scheduleNextFloor();
-    // Sleep for movement
+    if(!list_empty(&requestQueue)) { // If there is a request to service
+      nextFloorToVisit = list_first_entry(&requestQueue, RequestNode, requestList);
+
+      // Move elevator to this request's current floor then service the floor
+      moveToFloor(nextFloorToVisit->floorNum);
+      unloadPassengers();
+      loadPassengers();
+
+      // Delete the request
+      list_del_init(&nextFloorToVisit->requestList);
+      kfree(nextFloorToVisit);
+    }
+    printk("IM STILL ALIVE!!!\n");
     ssleep(2);
-    moveToFloor(osMagicElv.nextFloor);
-
-    // Sleep for load/unload
-    ssleep(1);
-    unloadPassengers();
-    loadPassengers();
   }
-
-  osMagicElv.state = STOPPED;
   return 0;
 }
-
-    /*
-        elevatorStatus == STOPPED
-        while(elevator is not empty)
-          moveToFloor(next floor)
-          unloadPassengers()
-
-          lastly, elevatorState == STOPPED
-    */
