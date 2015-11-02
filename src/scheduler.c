5a1c8af716e47571c482c3f4dd86d6cb7e9170c9
Ricardo Castilla <castillaricardo19@gmail.com>
2015-11-01 22:57:09 -0500

    fixed scheduler problem

--- src/scheduler.c
+++ src/scheduler.c
@@ -6,39 +6,23 @@
 #include <linux/slab.h>
 #include "elevator.h"
 
-
-// Add passenger's desired floor to the list of scheduled floors
-void scheduleRequest(int requestFloor) {
-  RequestNode *newRequest;
-
-  // Add the new request floor to the queue of floors to visit.
-  newRequest = kmalloc(sizeof(RequestNode), __GFP_WAIT | __GFP_IO | __GFP_FS);
-  newRequest->floorNum = requestFloor;
-  printk("Memory for newRequest successfully allocated\n");
-  list_add_tail(&newRequest->requestList, &requestQueue);
-  printk("newRequest added to request queue\n");
-}
-
-
+// Threaded function to handle elevator movement.
 int serviceRequests(void *data) {
-  RequestNode *nextFloorToVisit;
   ssleep(1);
 
   while(!kthread_should_stop()) {
-    if(!list_empty(&requestQueue)) { // If there is a request to service
-      nextFloorToVisit = list_first_entry(&requestQueue, RequestNode, requestList);
-
-      // Move elevator to this request's current floor then service the floor
-      moveToFloor(nextFloorToVisit->floorNum);
-      unloadPassengers();
-      loadPassengers();
-
-      // Delete the request
-      list_del_init(&nextFloorToVisit->requestList);
-      kfree(nextFloorToVisit);
-    }
-    printk("IM STILL ALIVE!!!\n");
+    // Determine the next floor to visit
+    osMagicElv.nextFloor = scheduleNextFloor();
+    // Sleep for movement
     ssleep(2);
+    moveToFloor(osMagicElv.nextFloor);
+
+    // Sleep for load/unload
+    ssleep(1);
+    unloadPassengers();
+    loadPassengers();
   }
+
+  osMagicElv.state = STOPPED;
   return 0;
 }
